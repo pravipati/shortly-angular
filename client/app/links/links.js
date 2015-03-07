5030814149a81e9933e5b42196adb65be64c7717
pravipati <psravipati@gmail.com>
2015-03-07 12:28:08 -0800

    Can sort and filter links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,7 +2,7 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
-  $scope.data = {};
+  $scope.data = [];
 
   console.log('in links Controller after a code request');
 
@@ -10,7 +10,7 @@ angular.module('shortly.links', [])
     Links.getLinks($scope.data)
     .then(function(links) {
       links.forEach(function(link) {
-         $scope.data[link.code] = link;
+         $scope.data.push(link);
       });
     })
     .catch(function(error) {
